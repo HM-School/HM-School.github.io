6dd333f033ad85f38e080fba357e6c44a30653fc
NingyMD <ningy.hm@gmail.com>
2025-01-08 13:51:51 -0800

    modified:   meraki/home.html 	modified:   primary/javaScript/Login-Register.js 	deleted:    primary/javaScript/server-acc.js-dissabled

--- primary/javaScript/Login-Register.js
+++ primary/javaScript/Login-Register.js
@@ -22,24 +22,3 @@ btnPopup.addEventListener('click', () => {
 iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 });
-
-submitBtn.addEventListener('click', async () => {
-    try {
-        // Adjust this path according to your actual API endpoint
-        const response = await fetch('../c#/Login-Register/Register/user'); 
-        const test = await response.text();
-
-        // Define the test1 function to check for null or empty values
-        function test1(text) {
-            if (text === null || text === "") {
-                return "The value could not be retrieved";
-            } else {
-                return text;
-            }
-        }
-
-        console.log(test1(test));
-    } catch (error) {
-        console.error("Failed to load data:", error);
-    }
-});
