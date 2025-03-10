dc08f53b2c7d15f94c7def8a5a6db012b7fbe4da
NingyMD <ningy.hm@gmail.com>
2025-03-10 10:46:52 -0700

    deleted:    public/ccs/Login.css 	modified:   public/ccs/footer.css 	new file:   public/ccs/nav-menu.css 	modified:   public/meraki/news.html 	modified:   public/scripts/Login-Register.js

--- public/scripts/Login-Register.js
+++ public/scripts/Login-Register.js
@@ -1,24 +1,12 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('#btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
-const submitBtn = document.querySelector('.btn');
+const nav_wrapper = document.querySelector('.nav-wrapper');
+const navmenu = document.querySelector('.nav-menu');
 
 
 
-registerLink.addEventListener('click', () => {
-    wrapper.classList.add('active');
+navmenu.addEventListener('click', () => {
+    nav_wrapper.classList.add('active');
 });
 
-loginLink.addEventListener('click', () => {
-    wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', () => {
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', () => {
-    wrapper.classList.remove('active-popup');
+navmenu.addEventListener('click', () => {
+    nav_wrapper.classList.remove('active');
 });
